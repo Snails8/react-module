7166a9da4a3f7a566423b6d0e5697e6026377c67
Kaito Suzuki <snails8d@gmail.com>
2022-09-14 19:55:11 +0900

    refactor:reload処理をカスタムフックに移動

---
+++ frontend/src/pages/StatePage/useReload.hooks.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export const useReload = (
+
+): string => {
+  const [reloadText, setReloadText] = useState<string>("") 
+  
+  // reload時値を復元する
+  useEffect(() => {
+    if (localStorage.getItem('reload') === "reloaded") {
+        setReloadText(localStorage.getItem('reload') as string)
+    }
+
+    localStorage.setItem('reload', "")
+  },[]);
+
+  // reload 直前に実行される
+  window.addEventListener('beforeunload', (e) => {
+    // console.log("reloadりろーど時に実行される")
+    localStorage.setItem('reload', "reloaded")
+  });
+
+  return reloadText
+}
