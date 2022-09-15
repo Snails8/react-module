e997ee27abee15e4a36385279bc5bb4a62b30ebe
Kaito Suzuki <snails8d@gmail.com>
2022-09-15 09:04:52 +0900

    add: loading spinner 作成

---
+++ frontend/src/components/atoms/LoadingSpinner/LoadingSpiner.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from './LoadingSpinner.module.css';
+
+export const LoadingSpinner: React.FC = ({
+
+}) => (
+    <div className={`${styles.spinner}`} />
+)
