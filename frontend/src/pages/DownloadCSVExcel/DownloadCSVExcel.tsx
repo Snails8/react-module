4c19ece64d767f448c3b2a25a517d0cac7c3dd1d
Kaito Suzuki <snails8d@gmail.com>
2022-09-30 08:41:20 +0900

    add: csv DL機能追加

---
+++ frontend/src/pages/DownloadCSVExcel/DownloadCSVExcel.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./DownloadCSVExcel.module.css";
+import ExcelJS from "exceljs";
+import { BasePage } from "../../components/templates/BasePage/BasePage";
+
+export const DownloadCSVExcel: React.FC = () => {
+  const handlerClickDownloadButton = async (e: 
+    React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    format: "xlsx" | "csv"
+  ) => {
+    e.preventDefault();
+
+    const workbook = new ExcelJS.Workbook();
+    workbook.addWorksheet("sheet1");
+    const worksheet = workbook.getWorksheet("sheet1");
+
+    worksheet.columns = [
+      { header: "ID", key: "id" },
+      { header: "作成日時", key: "createdAt" },
+      { header: "名前", key: "name" }
+    ];
+
+    worksheet.addRows([
+      {
+        id: "f001",
+        createdAt: 1629902208,
+        name: "りんご"
+      },
+      {
+        id: "f002",
+        createdAt: 1629902245,
+        name: "ぶとう"
+      },
+      {
+        id: "f003",
+        createdAt: 1629902265,
+        name: "ばなな"
+      }
+    ]);
+
+    const uint8Array = format === "xlsx" ? await workbook.xlsx.writeBuffer() : await workbook.csv.writeBuffer();
+    const blob = new Blob([uint8Array], { type: "application/octet-binary" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "sampleData." + format; //フォーマットによってファイル拡張子を変えている
+    a.click();
+    a.remove();
+  };
+
+  return (
+    <>
+      <BasePage>
+        <header>
+          <h1>データ出力</h1>
+        </header>
+        <button onClick={(e) => handlerClickDownloadButton(e, "xlsx")}>
+          Excel形式
+        </button>
+        <button onClick={(e) => handlerClickDownloadButton(e, "csv")}>
+          CSV形式
+        </button>
+      </BasePage>
+    </>
+  );
+};
