83d5758d49e1cbe470d95f6704751feb2c9fa54a
Kaito Suzuki <snails8d@gmail.com>
2022-10-17 16:18:05 +0900

    add: Logo component の作成

---
+++ frontend/src/components/atoms/Logo/Logo.tsx
@@ -0,0 +1,17 @@
+import styles from "./Logo.module.css";
+
+type LogoProps = {
+  logoPath: string
+}
+
+export const Logo = (
+  props: LogoProps
+)=> {
+  const {logoPath} = props;
+
+  return (
+    <div className={`${styles.base}`}>
+      <img src={logoPath} alt="icon" />
+    </div>
+  )
+}
