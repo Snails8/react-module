bdfea3a0ae32462893c24381e3ad7ff68e90bed3
Kaito Suzuki <snails8d@gmail.com>
2022-10-17 16:49:53 +0900

    add: userlogo component

---
+++ frontend/src/components/atoms/UserLogo/UserLogo.tsx
@@ -0,0 +1,17 @@
+import { memo } from "react";
+import { UserIconSvg } from "./UserIconSvg";
+
+type UserIconProps = {
+  height: number;
+  width: number;
+}
+
+export const UserIcon = memo((props: UserIconProps) => {
+  const {height, width} = props
+
+  return (
+    <div style={{ height:height, width: width }}>
+      <UserIconSvg />
+    </div>
+  )
+})
