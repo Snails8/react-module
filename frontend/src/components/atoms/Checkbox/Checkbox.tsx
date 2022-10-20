c7c621697c9ab4a4d55122c2be28889fbee51bfe
Kaito Suzuki <snails8d@gmail.com>
2022-10-20 11:07:06 +0900

    refactoring

--- frontend/src/components/atoms/Checkbox/Checkbox.tsx
+++ frontend/src/components/atoms/Checkbox/Checkbox.tsx
@@ -1,42 +1,37 @@
-import React, { useState } from "react";
+import { memo, useState } from "react";
 import styles from './Checkbox.module.css';
 
-export interface CheckBoxProps {
-    id?: string
-    label?: string
-    defaultValue?: boolean // default 値をレンダリング後表示したい場合は key を使う側で定義する ex) key={`input_${val}`}
-    disabled?: boolean
-    onChangeHandler: (check: boolean) => void
+type CheckboxProps = {
+  id?: string
+  label?: string
+  defaultValue?: boolean  // default 値をレンダリング後表示したい場合は key を使う側で定義する ex) key={`input_${val}`}
+  disabled?: boolean
+  onChangeHandler: (check: boolean) => void
 }
 
-export const CheckBox: React.FC<CheckBoxProps> = ({
-    id = '',
-    label = '',
-    defaultValue = false,
-    disabled = false,
-    onChangeHandler,
-}) => {
-    const [value, setValue] = useState(defaultValue);
+export const CheckBox = memo((props: CheckboxProps) => {
+  const {id='checkbox', label='', defaultValue=false, disabled=false, onChangeHandler} = props;
+  const [value, setValue] = useState(defaultValue);
 
-    return (
-        <div className={`${styles.container}` }>
-            <input
-                className={`${styles.checkbox}`}
-                type='checkbox'
-                id={id}
-                checked={value}
-                value={value.toString()}
-                disabled={disabled}
-                onChange={(e: any) => {
-                    if (disabled === true) return;
-                    
-                    setValue(e.target.checked);
-                    onChangeHandler(e.target.checked)
-                }}
-            />
-            {label !== '' && id !== '' ? 
-                <label>{label}</label> : null
-            }
-        </div>
-    )
-} 
+  return (
+    <div className={`${styles.container}` }>
+      <input
+        className={`${styles.checkbox}`}
+        type='checkbox'
+        id={id}
+        checked={value}
+        value={value.toString()}
+        disabled={disabled}
+        onChange={(e: any) => {
+          if (disabled === true) return;
+          
+          setValue(e.target.checked);
+          onChangeHandler(e.target.checked);
+        }}
+      />
+      {label !== '' && id !== '' ? 
+        <label>{label}</label> : null
+      }
+    </div>
+  );
+});
