1c1b775376afcbece499150fa7fe56619b08ca87
Kaito Suzuki <snails8d@gmail.com>
2022-10-21 21:51:14 +0900

    add: update users table

--- frontend/src/components/organisms/UsersTable/UsersTable.tsx
+++ frontend/src/components/organisms/UsersTable/UsersTable.tsx
@@ -1,42 +1,52 @@
-import React from "react";
-import { User } from "../../../hooks";
+import { memo } from "react";
+import { User, UserRole } from "../../../hooks/useUsers";
 
-import styles from "./UsersTable.module.css"
+import styles from "./UsersTable.module.css";
 
-// TODO::適切な型定義を行う
-export interface UsersTableProps {
-  /**
-  * ユーザーのリスト
-  */
+// TODO:: jsonに移行した際に修正
+const roleLabel = (role: string) => {
+  switch (role) {
+    case UserRole.Manager:
+      return '部門管理者';
+    case UserRole.Operator:
+      return '一般ユーザー';
+    default: // ロールは必ず付与されるので、このケースは実際には存在しない想定
+      return '';
+  }
+};
+
+type UsersTableProps = {
   users: User[]
 }
 
-export const UsersTable: React.FC<UsersTableProps> = ({
-  users
-}) => {
+export const UsersTable = memo((props: UsersTableProps) => {
+  const {users} = props;
+
   return (
-      <table className={`${styles.table}`}>
+      <table className={styles.table}>
         <thead>
           <tr>
-            <th className={`${styles.th}`} style={{ width: 100 }}>id</th>
-            <th className={`${styles.th}`} style={{ width: 100 }}>name</th>
-            <th className={`${styles.th}`} style={{ width: 100 }}>email</th>
+            <th className={styles.th} style={{ width: 100 }}>id</th>
+            <th className={styles.th} style={{ width: 100 }}>名前</th>
+            <th className={styles.th} style={{ width: 100 }}>email</th>
+            <th className={styles.th} style={{ width: 100 }}>ユーザー識別</th>
           </tr>
         </thead>
         <tbody>
-          { users != null? (
-              users.map((user) => {
-                return (
-                  <tr>
-                    <td className={`${styles.td}`}>{user.id}</td>
-                    <td className={`${styles.td}`}>{user.name}</td>
-                    <td className={`${styles.td}`}>{user.email}</td>
-                  </tr>
-                )
-              })
+          {users != null? (
+            users.map((user) => {
+              return (
+                <tr className={styles.table_row} key={user.id}>
+                  <td className={styles.td}>{user.id}</td>
+                  <td className={styles.td}>{user.name}</td>
+                  <td className={styles.td}>{user.email}</td>
+                  <td className={styles.td}>{roleLabel(user.role)}</td>
+                </tr>
+              );
+            })
             ): (<div>ユーザーが存在しません</div>)
           }
         </tbody>
       </table>    
-  )
-}
+  );
+});
--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { UsersTable } from "../../components/organisms/UsersTable/UsersTable";
 
+import UsersJson from "./../../hooks/Json/Users.json";
 import { useEffect, useState } from 'react';
 
 import {
@@ -25,22 +25,24 @@ export const Users:React.FC = () => {
   // TODO::コンパイルを通すため一時的にany
   // 解消方法はfetch の型がany になってしまうのでその修正
   // const users = useUsers();
-  const [users, setUsers] = useState<User[]>([]);
+  // const [users, setUsers] = useState<User[]>([]);
   
-  useEffect(() => {
-      fetch('http://localhost:7001/api/v1/users',{
-          method: 'GET'
-      })
-      .then(res => res.json())
-      .then((data) => {
-        setUsers(data.users)
-      },
-      (error) => {
-          console.log(error);
-          const errData = {
-          }
-      })
-  }, [])
+  // useEffect(() => {
+  //     fetch('http://localhost:7001/api/v1/users',{
+  //         method: 'GET'
+  //     })
+  //     .then(res => res.json())
+  //     .then((data) => {
+  //       setUsers(data.users)
+  //     },
+  //     (error) => {
+  //         console.log(error);
+  //         const errData = {
+  //         }
+  //     })
+  // }, [])
+
+  const users = UsersJson.users;
 
     return (
         <>
