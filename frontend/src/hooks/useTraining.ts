0c36261d441f3af157913c7ca543eba000286576
Kaito Suzuki <snails8d@gmail.com>
2022-10-23 16:47:49 +0900

    add: API定義作成

--- frontend/src/hooks/useTraining.ts
+++ frontend/src/hooks/useTraining.ts
@@ -1,4 +1,20 @@
 export interface TrainingCalendarData {
+  /**
+   * @type {number}
+   * @memberof TrainingCalendarData
+   */
+  id: number
+  
+    /**
+   * @type {string}
+   * @memberof TrainingCalendarData
+   */
+  name: string
+
+    /**
+   * @type {[key: string]: CalendarPerWeek}
+   * @memberof TrainingCalendarData
+   */
   calendar: {[key: string]: CalendarPerWeek}
 }
 
