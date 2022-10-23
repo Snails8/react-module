130832e3a9481b8bedaa300fa570ebe96b4c0031
Kaito Suzuki <snails8d@gmail.com>
2022-10-23 17:38:57 +0900

    add: sidebar の調整

--- frontend/src/components/organisms/Sidebar/Sidebar.tsx
+++ frontend/src/components/organisms/Sidebar/Sidebar.tsx
@@ -14,78 +14,88 @@ export const Sidebar = memo((props: SidebarProps) => {
   return (
     <div className={styles.container}>
       <Link to="/training">
-        <div className={ styles.sidebar_item }>
-          <div className={ styles.sidebar_text} >training</div>
+        <div className={ styles.link_container }>
+          <div className={ styles.label} >training</div>
         </div>
       </Link>
       <Link to="/fishes">
-        <div className={ styles.sidebar_item }>
-          <div className={ styles.sidebar_text} >fishes</div>
+        <div className={ styles.link_container }>
+          <div className={ styles.label} >fishes</div>
         </div>
       </Link>
       <Link to="/atoms">
-        <div className={ styles.sidebar_item }>
-          <div className={ styles.sidebar_text} >atoms 検証</div>
+        <div className={ styles.link_container }>
+          <div className={ styles.label} >atoms 検証</div>
         </div>
       </Link>
       <Link to="/users">
-        <div className={ styles.sidebar_item }>
-          <div className={ styles.sidebar_text} >ユーザー管理</div>
+        <div className={ styles.link_container }>
+          <div className={ styles.label} >ユーザー管理</div>
         </div>
       </Link>
       <Link to="/inspects">
-        <div className={styles.sidebar_item} onClick={() => setClickStatePage(!clickStatePage)}>
-          <div className={ styles.sidebar_item }>
-            <div className={ styles.sidebar_text} >hooks検証</div>
-          </div>
+        <div className={styles.link_container} onClick={() => setClickStatePage(!clickStatePage)}>
+          <div className={ styles.label} > hooks検証</div>
         </div>
       </Link>
       { clickStatePage &&
         <div>
           <Link to="/memo">
-            <div className={ styles.sidebar_item }>
-              <div className={ styles.sidebar_text} >memo検証</div>
+            <div className={ styles.link_container }>
+              <div className={styles.link_container}>
+                <div className={ styles.label}>memo検証</div>
+              </div>
             </div>
           </Link>
           <Link to="/useMemo">
-            <div className={ styles.sidebar_item }>
-              <div className={ styles.sidebar_text} >useMemo検証</div>
+            <div className={ styles.link_container }>
+              <div className={styles.link_container}>
+                <div className={ styles.label} >useMemo検証</div>
+              </div>
             </div>
           </Link>
           <Link to="/useCallBack">
-            <div className={ styles.sidebar_item }>
-              <div className={ styles.sidebar_text} >useCallBack検証</div>
+            <div className={ styles.link_container }>
+              <div className={styles.link_container}>
+                <div className={ styles.label} >useCallBack検証</div>
+              </div>
             </div>
           </Link>
           <Link to="/states">
-            <div className={ styles.sidebar_item }>
-              <div className={ styles.sidebar_text} >状態保持検証</div>
+            <div className={ styles.link_container }>
+              <div className={styles.link_container}>
+                <div className={ styles.label} >状態保持検証</div>
+              </div>
             </div>
           </Link>
         </div>
       }
       <Link to="/samples">
-        <div className={styles.sidebar_item} onClick={() => setClickStatePage(!clickSamplePage)}>
-          <div className={ styles.sidebar_item }>
-            <div className={ styles.sidebar_text} >実装 sample</div>
-          </div>
+        <div className={styles.link_container} onClick={() => setClickSamplePage(!clickSamplePage)}>
+          <div className={ styles.label} >{clickSamplePage ? "実装 sample　∨" : "実装 sample　∧"}</div>
         </div>
       </Link>
       { clickSamplePage && 
         <div>
           <Link to="/form">
-            <div className={ styles.sidebar_item }>
-              <div className={ styles.sidebar_text} >フォーム検証</div>
+            <div className={ styles.link_container }>
+              <div className={styles.link_container}>
+                <div className={ styles.label} >フォーム検証</div>
+              </div>
             </div>
           </Link>
           <Link to="/parent-tab">
-            <div className={ styles.sidebar_item }>
-              <div className={ styles.sidebar_text} >タブ間通信検証</div>
-            </div>
+            <div className={ styles.link_container }>
+              <div className={styles.link_container}>
+                <div className={ styles.label} >タブ間通信検証</div>
+              </div>
+            </div>  
           </Link>
           <Link to="/download-file">
-            <div className={ styles.sidebar_item }>
-              <div className={ styles.sidebar_text} >CSV・Excel 関連</div>
+            <div className={ styles.link_container }>
+              <div className={styles.link_container}>
+                <div className={ styles.label} >CSV・Excel 関連</div>
+              </div>
             </div>
           </Link>
         </div>
