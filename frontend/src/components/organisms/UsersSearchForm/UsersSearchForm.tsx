33aa51cd460a968775abbbbaf628e34194c53f89
Kaito Suzuki <snails8d@gmail.com>
2022-10-24 20:38:39 +0900

    add: get 検索 検証用のform作成

---
+++ frontend/src/components/organisms/UsersSearchForm/UsersSearchForm.tsx
@@ -0,0 +1,42 @@
+import { memo } from "react";
+import { Button } from "../../atoms/Button/Button";
+import { CheckBox } from "../../atoms/Checkbox/Checkbox";
+import { Input } from "../../atoms/Input/Input";
+import styles from "./UsersSearchForm.module.css";
+
+type UsersSearchFormProps = {
+  inputUserNameHandler: (value: string) => void
+  checkHandler: (checked: boolean) => void
+  handleClick: () => void
+}
+
+export const UsersSearchForm = memo((props: UsersSearchFormProps) => {
+  const { inputUserNameHandler, checkHandler, handleClick } = props;
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.input_container}>
+        <div className={styles.input}>
+          <label>名前</label>
+          <Input id="input" type="text" defaultValue="" width={300} height={35} padding={1} onChangeHandler={inputUserNameHandler} />
+        </div>
+        <div className={styles.input}>
+          <label>email</label>
+          <Input id="input" type="text" defaultValue="" width={300} height={35} padding={1} onChangeHandler={inputUserNameHandler} />
+        </div>
+      </div>
+      
+      <div className={styles.checkbox_container}>
+        <div className={styles.checkbox}>
+          <CheckBox id='checkbox' label="checkbox" onChangeHandler={checkHandler} />
+        </div>
+        <div className={styles.checkbox}>
+          <CheckBox id='checkbox' label="checkbox" onChangeHandler={checkHandler} />
+        </div>
+      </div>
+      <div className={styles.button_container}>
+        <Button colorType='primary' label="検索" width={100} height={35} handleClick={handleClick}/>
+      </div>
+    </div>
+  );
+});
--- frontend/src/components/templates/UsersTemplate/UsersTemplate.tsx
+++ frontend/src/components/templates/UsersTemplate/UsersTemplate.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { User } from "../../../hooks/useUsers";
+import { UsersSearchForm } from "../../organisms/UsersSearchForm/UsersSearchForm";
 import { UsersTable } from "../../organisms/UsersTable/UsersTable";
 import { Layout } from "../_Layout/Layout";
+import styles from "./UsersTemplate.module.css";
 
 type UsersTemplateProps = {
   users: User[]
@@ -10,9 +13,21 @@ type UsersTemplateProps = {
 export const UsersTemplate = (props: UsersTemplateProps) => {
   const users = props.users;
 
+  const [name, setName] = useState('');
+  const inputUserNameHandler = (value: string) => setName(value);
+
+  const handleClick = () => {
+    console.log('test');
+  };
+
+  const checkHandler = (checked: boolean) => console.log(checked);
+
   return (
     <Layout isContainerDesign={true}>
-    <Link to="/users/create">作成</Link>
+      <Link to="/users/create">作成</Link>
+      <div className={styles.search_container}>
+        <UsersSearchForm inputUserNameHandler={inputUserNameHandler} checkHandler={checkHandler} handleClick={handleClick} />
+      </div>
       <UsersTable 
         users={users}
       />  
