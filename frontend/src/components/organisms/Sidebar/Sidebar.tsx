d9950dd2379f9b84435be982ed9f86ddaf0274f9
Kaito Suzuki <snails8d@gmail.com>
2022-10-24 19:47:46 +0900

    add: sidebar のデザインをresponsiveに変更

--- frontend/src/components/organisms/Sidebar/Sidebar.tsx
+++ frontend/src/components/organisms/Sidebar/Sidebar.tsx
@@ -11,58 +11,60 @@ export const Sidebar = memo((props: SidebarProps) => {
   const [clickStatePage, setClickStatePage] = useState(currentPath.startsWith('/inspects'));
   const [clickSamplePage, setClickSamplePage] = useState(currentPath.startsWith('/samples'));
   
+  const activeColor = "#f7f7f7";
+
   return (
     <div className={styles.container}>
-      <Link to="/training">
-        <div className={ styles.link_container }>
+      <Link to="/training" className={styles.link}>
+        <div className={ styles.link_container } style={currentPath === '/training' ? {backgroundColor: activeColor} : {}}>
           <div className={ styles.label} >training</div>
         </div>
       </Link>
-      <Link to="/fishes">
-        <div className={ styles.link_container }>
+      <Link to="/fishes" className={styles.link}>
+        <div className={ styles.link_container } style={currentPath === '/fishes' ? {backgroundColor: activeColor} : {}}>
           <div className={ styles.label} >fishes</div>
         </div>
       </Link>
-      <Link to="/atoms">
-        <div className={ styles.link_container }>
+      <Link to="/atoms" className={styles.link}>
+        <div className={ styles.link_container } style={currentPath === '/atoms' ? {backgroundColor: activeColor} : {}}>
           <div className={ styles.label} >atoms 検証</div>
         </div>
       </Link>
-      <Link to="/users">
-        <div className={ styles.link_container }>
+      <Link to="/users" className={styles.link}>
+        <div className={ styles.link_container } style={currentPath === '/users' ? {backgroundColor: activeColor} : {}}>
           <div className={ styles.label} >ユーザー管理</div>
         </div>
       </Link>
-      <Link to="/inspects">
+      <Link to="/inspects" className={styles.link} style={currentPath === '/inspects' ? {backgroundColor: activeColor} : {}}>
         <div className={styles.link_container} onClick={() => setClickStatePage(!clickStatePage)}>
           <div className={ styles.label} > hooks検証</div>
         </div>
       </Link>
       { clickStatePage &&
         <div>
-          <Link to="/memo">
-            <div className={ styles.link_container }>
+          <Link to="/memo" className={styles.link}>
+            <div className={ styles.link_container } style={currentPath === '/memo' ? {backgroundColor: activeColor} : {}}>
               <div className={styles.link_container}>
                 <div className={ styles.label}>memo検証</div>
               </div>
             </div>
           </Link>
-          <Link to="/useMemo">
-            <div className={ styles.link_container }>
+          <Link to="/useMemo" className={styles.link}>
+            <div className={ styles.link_container } style={currentPath === '/useMemo' ? {backgroundColor: activeColor} : {}}>
               <div className={styles.link_container}>
                 <div className={ styles.label} >useMemo検証</div>
               </div>
             </div>
           </Link>
-          <Link to="/useCallBack">
-            <div className={ styles.link_container }>
+          <Link to="/useCallBack" className={styles.link}>
+            <div className={ styles.link_container } style={currentPath === '/useCallBack' ? {backgroundColor: activeColor} : {}}>
               <div className={styles.link_container}>
                 <div className={ styles.label} >useCallBack検証</div>
               </div>
             </div>
           </Link>
-          <Link to="/states">
-            <div className={ styles.link_container }>
+          <Link to="/states" className={styles.link}>
+            <div className={ styles.link_container } style={currentPath === '/states' ? {backgroundColor: activeColor} : {}}>
               <div className={styles.link_container}>
                 <div className={ styles.label} >状態保持検証</div>
               </div>
@@ -70,29 +72,29 @@ export const Sidebar = memo((props: SidebarProps) => {
           </Link>
         </div>
       }
-      <Link to="/samples">
+      <Link to="/samples" className={styles.link}>
         <div className={styles.link_container} onClick={() => setClickSamplePage(!clickSamplePage)}>
           <div className={ styles.label} >{clickSamplePage ? "実装 sample　∨" : "実装 sample　∧"}</div>
         </div>
       </Link>
       { clickSamplePage && 
         <div>
-          <Link to="/form">
-            <div className={ styles.link_container }>
+          <Link to="/form" className={styles.link}>
+            <div className={ styles.link_container } style={currentPath === '/form' ? {backgroundColor: activeColor} : {}}>
               <div className={styles.link_container}>
                 <div className={ styles.label} >フォーム検証</div>
               </div>
             </div>
           </Link>
-          <Link to="/parent-tab">
-            <div className={ styles.link_container }>
+          <Link to="/parent-tab" className={styles.link}>
+            <div className={ styles.link_container } style={currentPath === '/parent-tab' ? {backgroundColor: activeColor} : {}}>
               <div className={styles.link_container}>
                 <div className={ styles.label} >タブ間通信検証</div>
               </div>
             </div>  
           </Link>
-          <Link to="/download-file">
-            <div className={ styles.link_container }>
+          <Link to="/download-file" className={styles.link}>
+            <div className={ styles.link_container } style={currentPath === '//download-file' ? {backgroundColor: activeColor} : {}}>
               <div className={styles.link_container}>
                 <div className={ styles.label} >CSV・Excel 関連</div>
               </div>
