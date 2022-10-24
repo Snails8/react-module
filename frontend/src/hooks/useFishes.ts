f21cdc2fe81e0c28f41b313e46c854df30671853
Kaito Suzuki <snails8d@gmail.com>
2022-10-24 20:38:02 +0900

    add; 環境変数でデータを切り替えられるように調整

--- frontend/src/hooks/useFishes.ts
+++ frontend/src/hooks/useFishes.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import FishData from "./Json/Fishes.json";
 
 export interface Fish {
     /**
@@ -20,28 +21,42 @@ export interface Fish {
      * @type {string}
      * @memberof Fish
      */
-    email: string;
+    type: string;
+
+    habitat: string
+
+    endangeredStatus: string
 }
 
 export const useFishes = () => {
-    const [fishes, setFishes] = useState<Fish[]>([]);
+  const fishes = process.env.IS_BACKEND ? getFishesFromAPI() : getFishesFromJson();
 
-    useEffect(() => {
-        fetch('http://localhost:7001/api/v1/fishes',{
-            method: 'GET'
-        })
-        .then(res => res.json())
-        .then((data) => {
-            setFishes(data);
-        },
-        (error) => {
-            console.log(error);
-            const errData = {
-            };
-        });
-    }, []);
+  return {
+    fishes: fishes
+  };
+};
 
-    return {
-        fishes: fishes
-    };
+const getFishesFromJson = (): Fish[] => {
+  return FishData.fishes; 
+};
+
+const getFishesFromAPI = (): Fish[] => {
+  const [fishes, setFishes] = useState<Fish[]>([]);
+
+  useEffect(() => {
+      fetch('http://localhost:7001/api/v1/fishes',{
+          method: 'GET'
+      })
+      .then(res => res.json())
+      .then((data) => {
+          setFishes(data);
+      },
+      (error) => {
+          console.log(error);
+          const errData = {
+          };
+      });
+  }, []);
+
+  return fishes;
 };
