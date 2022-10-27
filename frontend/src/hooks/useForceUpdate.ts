86fce44ea2d09d02c24161754085696e5a57c81e
Kaito Suzuki <snails8d@gmail.com>
2022-10-27 14:57:03 +0900

    add: forceupdate のhooks作成

---
+++ frontend/src/hooks/useForceUpdate.ts
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+/**
+ * 強制レンダリングを起こす際に使用するhooks
+ * usage: forceUpdate(v => !v);
+ * @returns 
+ */
+export const useForceUpdate = () => {
+  return useState(false)[1];
+};
