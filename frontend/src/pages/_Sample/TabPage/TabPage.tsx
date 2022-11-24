fe916a3b46881abea5adebea78e372297b06ed1a
Kaito Suzuki <snails8d@gmail.com>
2022-11-24 14:14:15 +0900

    add: tab作成

--- frontend/src/pages/_Sample/TabPage/TabPage.tsx
+++ frontend/src/pages/_Sample/TabPage/TabPage.tsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
 import { Layout } from "../../../components/templates/_Layout/Layout";
 import styles from "./TabPage.module.css";
 
+
 export const TabPage = () => {
+  const [selected, setSelected] = useState(0);
+
   return (
     <Layout isContainerDesign>
-      <div>
+      <div className={styles.container}>
 
-        <ul></ul>
+        <ul className={styles.tab}>
+          <li className={styles.tab__item}>
+            <span className={styles.tab__link} onClick={() => setSelected(0)} style={selected === 0 ? {backgroundColor: "green"}: {}}>タブ1</span>
+          </li>
+          <li className={styles.tab__item}>
+            <span className={styles.tab__link} onClick={() => setSelected(1)} style={selected === 1 ? {backgroundColor: "green"}: {}}>タブ2</span>
+          </li>
+          <li className={styles.tab__item}>
+            <span className={styles.tab__link} onClick={() => setSelected(2)} style={selected === 2 ? {backgroundColor: "green"}: {}}>タブ3</span>
+          </li>
+        </ul>
+        <div className={styles.tab__body}>
+          <PageContent selected={selected} />
+        </div>
       
       </div>
     </Layout>
   );
+};
+
+type PageContentProps = {
+  selected: number
+}
+
+const PageContent = (props: PageContentProps) => {
+  const {selected} = props;
+
+  switch (selected) {
+    case 0:
+      return <span>タブコンテンツ1を動的に切り替える</span>;
+    case 1:
+      return <span>タブコンテンツ2を動的に切り替える</span>;
+    case 2:
+      return <span>タブコンテンツ3を動的に切り替える</span>;
+    default:
+      return <span>test</span>;
+  };
 };
