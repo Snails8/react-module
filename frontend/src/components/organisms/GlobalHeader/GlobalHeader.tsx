836e8d69a46333e88105789660e0cba6a624acb5
Kaito Suzuki <snails8d@gmail.com>
2022-12-30 06:55:34 +0900

    add: user menu

--- frontend/src/components/organisms/GlobalHeader/GlobalHeader.tsx
+++ frontend/src/components/organisms/GlobalHeader/GlobalHeader.tsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom';
 import { Logo } from '../../atoms/Logo/Logo';
 import styles from './GlobalHeader.module.css';
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import { User, UserRole } from '../../../hooks';
+import { UserMenu } from '../UserMenu/UserMenu';
 
 type HeaderProps = {
   pageTitle?: string;
 };
 
 export const GlobalHeader = memo((props: HeaderProps) => {
+  const [visible, setVisible] = useState(false);
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+  const user: User = {
+    id: 1,
+    role: UserRole.Manager,
+    name: 'hoge',
+    email: 'sample@sample.com',
+  };
+
   return (
     <div className={styles.container}>
       <Link to="/">
@@ -18,11 +31,14 @@ export const GlobalHeader = memo((props: HeaderProps) => {
         </div>
       </Link>
 
-      <div className={styles.user_container}>
+      <div className={styles.user_container} onClick={handleClick}>
         <div className={styles.user_icon}>
           {/* <UserIcon height={30} width={30} /> */}
         </div>
         <div className={styles.user_name}>テスト太郎</div>
+        <div className={styles.user_menu}>
+          { visible && <UserMenu user={user}/> }
+        </div>
       </div>
     </div>
   );
