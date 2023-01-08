5911917f9146e452ae4f9b194e84909cb7f6713a
Kaito Suzuki <snails8d@gmail.com>
2023-01-08 09:16:14 +0900

    add: timeout hooks

---
+++ frontend/src/hooks/useTimeout.ts
@@ -0,0 +1,50 @@
+import { useEffect, useLayoutEffect, useRef } from 'react'
+
+function useTimeout(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback)
+
+  // Remember the latest callback if it changes.
+  useLayoutEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the timeout.
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return
+    }
+
+    const id = setTimeout(() => savedCallback.current(), delay)
+
+    return () => clearTimeout(id)
+  }, [delay])
+}
+
+export default useTimeout
+
+/**
+ * usage
+
+export default function Component() {
+  const [visible, setVisible] = useState(true)
+
+  const hide = () => setVisible(false)
+
+  useTimeout(hide, 5000)
+
+  return (
+    <div>
+      <p>
+        {visible
+          ? "I'm visible for 5000ms"
+          : 'You can no longer see this content'}
+      </p>
+    </div>
+  )
+}
+
+ * 
+ */
+
