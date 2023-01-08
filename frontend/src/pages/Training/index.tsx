6873ad505d71fe885a7a6f8b97ba2211a71d020a
Kaito Suzuki <snails8d@gmail.com>
2023-01-08 14:50:35 +0900

    fix: 参照でエラーが起きていた問題の修正

--- frontend/src/pages/Training/index.tsx
+++ frontend/src/pages/Training/index.tsx
@@ -2,7 +2,7 @@ import { TrainingCalendar } from '../../components/organisms/TrainingCalendar/Tr
 import { Layout } from '../../components/templates/_Layout/Layout';
 
 export const Training = () => {
-  const trainingData = require('../../mockData/json/Training.json');
+  const trainingData = require('../../mockData/json/Training.json').training;
   const doShowDetail = false;
 
   return (
