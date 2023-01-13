0d6bb4d2b13c585ef2d3392d614a1d90ac10c6e7
Kaito Suzuki <snails8d@gmail.com>
2023-01-14 08:47:10 +0900

    refactor: 見通しをよくするためにcomponentの共通化

--- frontend/src/components/organisms/Atoms/index.tsx
+++ frontend/src/components/organisms/Atoms/index.tsx
@@ -6,99 +6,83 @@ import { LoadingSpinner } from '../../atoms/LoadingSpinner/LoadingSpinner';
 import { Logo } from '../../atoms/Logo/Logo';
 import { RadioButton } from '../../atoms/RadioButton/RadioButton';
 import { Textarea } from '../../atoms/Textarea/Textarea';
-import { Layout } from '../../templates/_Layout/Layout';
 import { useTypeConverter } from '../../../hooks/useTypeConverter';
 import styles from './Atoms.module.css';
+import { ReactNode } from 'react';
+
+type ContentProps = {
+  label: string,
+  children: ReactNode,
+}
+
+const Content = (props: ContentProps) => {
+  const {label, children} = props;
+  return (
+    <div className={styles.content}>
+      <h5>{label}</h5>
+      {children}
+    </div>
+  );
+};
 
 export const Atoms = () => {
   const hoge = useTypeConverter();
 
   return (
     <>
-      <Layout isContainerDesign>
-        <h2>form Item</h2>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <h5>button</h5>
-            <Button
-              label="label"
-              handleClick={() => console.log('clicked')}
-            />
-          </div>
-          <div className={styles.content}>
-            <h5>checkbox</h5>
-            <CheckBox
-              id="checkbox"
-              label="checkbox"
-              onChangeHandler={() => console.log('checkbox!')}
-            />
-          </div>
-          <div className={styles.content}>
-            <h5>radio</h5>
-            <RadioButton
-              id="radio"
-              label="radio"
-              onChangeHandler={() => console.log('radio!')}
-            />
-          </div>
-          <div className={styles.content}>
-            <h5>input</h5>
-            <Input
-              type="text"
-              id="text"
-              defaultValue="sample"
-              onChangeHandler={() => console.log('text')}
-            />
-          </div>
-          <div className={styles.content}>
-            <h5>textarea</h5>
-            <Textarea
-              id="text"
-              defaultValue={"sample\ntest"}
-              onChangeHandler={() => console.log('text')}
-            />
-          </div>
-        </div>
+      <h2>form Item</h2>
+      <div className={styles.item}>
+        <Content label="button">
+          <Button label="label" handleClick={() => console.log('clicked')}/>
+        </Content>
 
-        <div className={styles.item}>
-          <div className={styles.content}>
-              <h5>LoadingSpinner</h5>
-              <LoadingSpinner />
-            </div>
-          <div className={styles.content}>
-            <h5>Label</h5>
-            <Label text="label" color="green" width={100} />
-          </div>
-          <div className={styles.content}>
-            <h5>Logo</h5>
-            <Logo />
-          </div>
-          <div className={styles.content}>
-            <h5>Label2</h5>
-            nothing
-          </div>
-          <div className={styles.content}>
-            <h5>ErrorPanel</h5>
-            nothing
-          </div>
-          <div className={styles.content}>
-            <h5>Logo</h5>
-            nothing
-          </div>
-          <div className={styles.content}>
-            <h5>Notification field</h5>
-            nothing
-          </div>
-          <div className={styles.content}>
-            <h5>toast</h5>
-            nothing
-          </div>
-          <div className={styles.content}>
-            <h5>card</h5>
-            nothing
-          </div>
-        </div>
-      </Layout>
+        <Content label="checkbox">
+          <CheckBox
+            id="checkbox"
+            label="checkbox"
+            onChangeHandler={() => console.log('checkbox!')}
+          />
+        </Content>
+
+        <Content label="radio">
+          <RadioButton
+            id="radio"
+            label="radio"
+            onChangeHandler={() => console.log('radio!')}
+          />
+        </Content>
+
+        <Content label="input">
+          <Input
+            type="text"
+            id="text"
+            defaultValue="sample"
+            onChangeHandler={() => console.log('text')}
+          />
+        </Content>
+
+        <Content label="textarea">
+          <Textarea id="text" defaultValue={"sample\ntest"} onChangeHandler={() => console.log('text')}/>
+        </Content>
+      </div>
+
+      <div className={styles.item}>
+        <Content label="LoadingSpinner">
+          <LoadingSpinner />
+        </Content>
+
+        <Content label="Label">
+          <Label text="label" color="green" width={100} />
+        </Content>
+
+        <Content label="LoadingSpinner">
+          <Logo />
+        </Content>
+
+        <Content label="Label">
+          <Label text="label" color="green" width={100} />
+        </Content>
+      </div>
     </>
   );
 };
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import { createSearchParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/atoms/Button/Button';
 import { Container } from '../../components/atoms/Container/Container';
+import { Atoms } from '../../components/organisms/Atoms';
 import { LoadingTemplate } from '../../components/templates/LoadingTemplate/LoadingTemplate';
 import { Layout } from '../../components/templates/_Layout/Layout';
 import { APIPostTest, APIGetTest, APIPutTest } from '../../endpoint';
@@ -48,40 +49,42 @@ export const Dashboard: React.FC = () => {
       <Layout isContainerDesign>
         <Container type='center'>
           <div>
-          <h1>Sample Home</h1>
-          <h1>{data ? "BEと接続中": "BEと未接続"}</h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to="training">Training</Link>
-              </li>
-              <li>
-                <Link to="page2">Sample Page2</Link>
-              </li>
-              <li>
-                <Link to="page2?name=鈴木&type=test">
-                  Sample Page2 With Query1
-                </Link>
-              </li>
-              <li>
-                <Link to={`page2?${params}`}>Sample Page2 With Query2</Link>
-              </li>
-              <li>
-                <Link to="page3">Sample Page3 props</Link>
-              </li>
-              <li>
-                <Link to="page4">Sample Page4</Link>
-              </li>
-            </ul>
-
-            <button onClick={() => navigate('page1')}>SamplePage1</button>
-          </nav>
-
-          <div style={{ display: 'flex', }}>
-            <Button label='post' handleClick={handleSubmitPost}/>
-            <Button label='put' handleClick={handleSubmitPut}/>
+            <h1>Sample Home</h1>
+            <h1>{data ? "BEと接続中": "BEと未接続"}</h1>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="training">Training</Link>
+                </li>
+                <li>
+                  <Link to="page2">Sample Page2</Link>
+                </li>
+                <li>
+                  <Link to="page2?name=鈴木&type=test">
+                    Sample Page2 With Query1
+                  </Link>
+                </li>
+                <li>
+                  <Link to={`page2?${params}`}>Sample Page2 With Query2</Link>
+                </li>
+                <li>
+                  <Link to="page3">Sample Page3 props</Link>
+                </li>
+                <li>
+                  <Link to="page4">Sample Page4</Link>
+                </li>
+              </ul>
+    
+              <button onClick={() => navigate('page1')}>SamplePage1</button>
+            </nav>
+    
+            <div style={{ display: 'flex', }}>
+              <Button label='post' handleClick={handleSubmitPost}/>
+              <Button label='put' handleClick={handleSubmitPut}/>
+            </div>
           </div>
-          </div>
+
+          <Atoms />
         </Container>
       </Layout>
     </>
