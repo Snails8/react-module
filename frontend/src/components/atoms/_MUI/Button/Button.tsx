2b5932a5df65381281d27710a5dc5cad21af310a
Kaito Suzuki <snails8d@gmail.com>
2023-01-14 15:28:58 +0900

    add: カラースタイルが選択できるように修正

--- frontend/src/components/atoms/_MUI/Button/Button.tsx
+++ frontend/src/components/atoms/_MUI/Button/Button.tsx
@@ -2,22 +2,23 @@ import { Button } from "@mui/material";
 import { FC } from "react";
 
 type ButtonProps = {
-  handleClick: (() => void) |  ((e: React.MouseEvent<any>) => any),
   label: string,
+  handleClick: (() => void) |  ((e: React.MouseEvent<any>) => any),
+  variant?: "text" | "contained" | "outlined"
   color?: "primary" | "secondary" | "success" | "error",
   disabled?: boolean,
 };
 
-// primaryのcolorが上書きされているので使うときは注意する
 export const MUIButton: FC<ButtonProps> = ({
   handleClick, 
   label, 
+  variant = "contained",
   color = "primary", 
   disabled=false 
 }) => {
   return (
     <Button
-      variant="contained"
+      variant={variant}
       color={color}
       onClick={handleClick}
       disabled={disabled}
