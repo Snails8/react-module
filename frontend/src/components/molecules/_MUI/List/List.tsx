0e21e1fb0dbb7b7d8bf7006981546dfcf10cb4ec
Kaito Suzuki <snails8d@gmail.com>
2023-01-19 09:40:24 +0900

    add: simple List

---
+++ frontend/src/components/molecules/_MUI/List/List.tsx
@@ -0,0 +1,38 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+
+import ListItemButton from '@mui/material/ListItemButton';
+
+type ListMUIprops = {
+  list: any[],
+  listStyle?: {}
+}
+
+// https://mui.com/material-ui/react-list/
+export const ListMUI = (props: ListMUIprops) => {
+  const {list, listStyle} = props;
+
+  return (
+    <List>
+      {list.map((value: any, idx: number) => (
+        <ListItem key={idx} disablePadding>
+          <ListItemButton sx={listStyle} >
+            <ListItemText primary={value} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+/**
+ * usage:
+ 
+  const listStyle = {
+    border: "1px solid silver",
+    minHeight: 80,
+  }
+  return (
+    <ListMUI list={['a', 'b,', 'c', 'd']} listStyle={listStyle}/>
+  )
+
+ */
