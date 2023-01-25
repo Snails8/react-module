41a2304d728253a389bb0f567195862f3841142f
Kaito Suzuki <snails8d@gmail.com>
2023-01-25 21:06:38 +0900

    add: debounce hooks

---
+++ frontend/src/hooks/useDebounce.ts
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from "react";
+
+function useDebounce(fn: () => any, ms: number = 0, args: any[] = []) {
+  useEffect(() => {
+    const handle = setTimeout(fn.bind(null, args), ms);
+
+    return () => {
+      clearTimeout(handle);
+    };
+  }, args);
+};
+
+export default function useDebouncedQuery(onChange: (query: string) => void) {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useDebounce(() => {
+      onChange(searchQuery);
+    },
+    500,
+    [searchQuery]
+  );
+
+  const clearQuery = useCallback(() => {
+    setSearchQuery('');
+  }, []);
+
+  return { searchQuery, setSearchQuery, clearQuery };
+}
+
+/**
+usage
+
+  const loadSuggestions = (query: string) => { ... }
+  const { searchQuery, setSearchQuery } = useDebouncedQuery(loadSuggestions);
+
+  return (
+    <Input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+  )
+
+ */
