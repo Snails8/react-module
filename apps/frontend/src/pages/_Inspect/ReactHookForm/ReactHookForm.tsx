269098dc1e3ef5648816b9430b9939964a15eaa0
Kaito Suzuki <snails8d@gmail.com>
2023-07-31 21:37:36 +0900

    add: text

--- apps/frontend/src/pages/_Inspect/ReactHookForm/ReactHookForm.tsx
+++ apps/frontend/src/pages/_Inspect/ReactHookForm/ReactHookForm.tsx
@@ -6,9 +6,10 @@ import { FormSample1 } from "./components/FormSample";
 export const ReactHookFormPage = () => {
   return (
     <div>
-      <p>入力と同時にエラーが出ない</p>
-      <p>Form-Controlで監視していないため。</p>
+      <p>基本的な使い方: useForm とonSubmitをformに渡して振る舞いはmodeが決める</p>
+      <p>MUI等のライブラリと連携させる場合や、特殊な入出力を扱う場合、フォームの状態に基づく動的な挙動を持つコンポーネントを扱う場合、ユニークチェックのような非同期バリデーションを行う場合、controlを使用する</p>
       <FormSample1 />
+
 
     </div>
   );
